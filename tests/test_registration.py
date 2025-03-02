5ec1374d5bf4fa2481937c604a48a651f17f9fae
DmitryAsl <aslamovdima10@yandex.ru>
2025-03-02 23:05:21 +0300

    add marks

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,9 +1,15 @@
+import allure
+from allure import step
+import pytest
 from jsonschema import validate
 from helpers.load_schema import load_schema
 from helpers.api_client import api_request
 from tests.config import BASE_URL
 from helpers.data_provider import DataProvider
 
+@pytest.mark.positive
+@pytest.mark.api
+@allure.title('Тест на проверку регистрации нового пользователя')
 def test_register_new_user():
     param = DataProvider.get('', 'registration', 'new_user')
     user_id = None
@@ -11,24 +17,30 @@ def test_register_new_user():
     schema_for_validate = load_schema('post_register_user')
 
     try:
-        response = api_request(method='POST', url=url, json=param['body'])
-        response_json = response.json()
+        with step('Запрос на регистрацию нового пользователя'):
+            response = api_request(method='POST', url=url, json=param['body'])
+            response_json = response.json()
 
-        assert response.status_code == param['status_code']
-        user_id = response_json['id']
-        validate(instance=response_json, schema=schema_for_validate)
+        with step('Проверка результата'):
+            assert response.status_code == param['status_code']
+            user_id = response_json['id']
+            validate(instance=response_json, schema=schema_for_validate)
 
     finally:
-        # если пользователь создан, нужно удолить ТД для следующего прогона (представляем, что api работает логично)
         if user_id:
-            api_request(method='DELETE', url=f'{BASE_URL}/api/users/{user_id}')
+            with step('Удаление пользователя для следующего прогона'):
+                api_request(method='DELETE', url=f'{BASE_URL}/api/users/{user_id}')
 
-
+@pytest.mark.negative
+@pytest.mark.api
+@allure.title('Тест на проверку регистрацию пользователя без данных')
 def test_negative_register_new_empty_user():
     param = DataProvider.get('', 'registration', 'new_empty_user')
     url = f'{BASE_URL}/api/users'
 
-    response = api_request(method='POST', url=url)
+    with step('Запрос на регистрацию пользователя без body'):
+        response = api_request(method='POST', url=url)
 
-    # тест ожидаемо будет падать в ошибку, т.к. текущее api позволяет создать пользователя без данных
-    assert response.status_code == param['status_code'], f'Пользователь с пустыми данными не должен создаваться.'
+    with step('Проверка результата'):
+        # тест ожидаемо будет падать в ошибку, т.к. текущее api позволяет создать пользователя без данных
+        assert response.status_code == param['status_code'], f'Пользователь с пустыми данными не должен создаваться.'
--- tests/test_user_management.py
+++ tests/test_user_management.py
@@ -1,67 +1,95 @@
+import allure
+from allure import step
+import pytest
 from jsonschema import validate
 from helpers.load_schema import load_schema
 from helpers.api_client import api_request
 from tests.config import BASE_URL
 from helpers.data_provider import DataProvider
 
+
+@pytest.mark.positive
+@pytest.mark.api
+@allure.title('Тест на проверку получения данных пользователя')
 def test_get_single_user():
     param = DataProvider.get('', 'user_management', 'get_user')
     schema_for_validate = load_schema('get_single_user')
     url = f'{BASE_URL}/api/users/{param["user_id"]}'
 
-    response = api_request(method='GET', url=url)
-    response_json = response.json()
+    with step('Запрос на получение данных пользователя по id'):
+        response = api_request(method='GET', url=url)
+        response_json = response.json()
 
-    assert response.status_code == param["status_code"]
-    validate(instance=response_json, schema=schema_for_validate)
-    assert response_json['data']['id'] == param["user_id"]
+    with step('Проверка результата'):
+        assert response.status_code == param["status_code"]
+        validate(instance=response_json, schema=schema_for_validate)
+        assert response_json['data']['id'] == param["user_id"]
 
 
+@pytest.mark.positive
+@pytest.mark.api
+@allure.title('Тест на проверку создания пользователя')
 def test_post_create_user():
     param = DataProvider.get('', 'user_management', 'post_create_user')
     schema_for_validate = load_schema('post_create_user')
     url = f'{BASE_URL}/api/users'
 
-    response = api_request(method='POST', url=url, json=param['body'])
-    response_json = response.json()
+    with step('Запрос на создание пользователя'):
+        response = api_request(method='POST', url=url, json=param['body'])
+        response_json = response.json()
 
-    assert response.status_code == param['status_code']
-    validate(instance=response_json, schema=schema_for_validate)
-    assert response_json['name'] == param['body']['name']
-    assert response_json['job'] == param['body']['job']
+    with step('Проверка результата'):
+        assert response.status_code == param['status_code']
+        validate(instance=response_json, schema=schema_for_validate)
+        assert response_json['name'] == param['body']['name']
+        assert response_json['job'] == param['body']['job']
 
-
+@pytest.mark.positive
+@pytest.mark.api
+@allure.title('Тест на проверку обновления пользователя')
 def test_put_update_user():
     param = DataProvider.get('', 'user_management', 'put_update_user')
 
     schema_for_validate = load_schema('put_update_user')
     url = f'{BASE_URL}/api/users/{param["user_id"]}'
 
-    response = api_request(method='PUT', url=url, json=param['body'])
-    response_json = response.json()
+    with step('Запрос на обновление данных'):
+        response = api_request(method='PUT', url=url, json=param['body'])
+        response_json = response.json()
 
-    assert response.status_code == param['status_code']
-    validate(instance=response_json, schema=schema_for_validate)
-    assert response_json['name'] == param['body']['name']
-    assert response_json['job'] == param['body']['job']
+    with step('Проверка результата'):
+        assert response.status_code == param['status_code']
+        validate(instance=response_json, schema=schema_for_validate)
+        assert response_json['name'] == param['body']['name']
+        assert response_json['job'] == param['body']['job']
 
 
+@pytest.mark.positive
+@pytest.mark.api
+@allure.title('Тест на проверку удаления пользователя')
 def test_delete_user():
     param = DataProvider.get('', 'user_management', 'delete_user')
     url = f'{BASE_URL}/api/users/{param["user_id"]}'
 
-    response = api_request(method='DELETE', url=url)
+    with step('Запрос на удаление пользователя по id'):
+        response = api_request(method='DELETE', url=url)
 
-    assert response.status_code == param['status_code']
-    assert not response.text
+    with step('Проверка результата'):
+        assert response.status_code == param['status_code']
+        assert not response.text
 
 
+@pytest.mark.negative
+@pytest.mark.api
+@allure.title('Тест на проверку получения данных по несуществующему пользователю')
 def test_negative_get_not_found_user():
     param = DataProvider.get('', 'user_management', 'get_not_found_user')
     url = f'{BASE_URL}/api/users/{param["user_id"]}'
 
-    response = api_request(method='GET', url=url)
-    response_json = response.json()
+    with step('Запрос на получение данных несуществующего пользователя'):
+        response = api_request(method='GET', url=url)
+        response_json = response.json()
 
-    assert response.status_code == param['status_code']
-    assert response_json == {}
+    with step('Проверка результата'):
+        assert response.status_code == param['status_code']
+        assert response_json == {}
